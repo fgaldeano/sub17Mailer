364176164b99b05da2dea99f40539d87d3de0d24
Facundo Galdeano <fgaldeano@gmail.com>
2018-08-30 21:07:41 -0300

    Reduced code by using arrow functions of ES6. Improved selector for ManagerUrl

--- src/scripts/youthmail.js
+++ src/scripts/youthmail.js
@@ -1,113 +1,87 @@
-const normalUrl = 'https://drive.google.com/open?id=1K6zS2XX1adT0wgBDF-7K4DB79Uyo7RdUqJP_6RYzK44';
-const bestNormalUrl = 'https://drive.google.com/open?id=16d9QdobMJXkV_50qjcGQc_FDSWp4MQPhC04HULZKBeI';
-const bestNoLeagueUrl = 'https://drive.google.com/open?id=1NKpYDpocY2zp8EBLIZF_OjzpB-rXMeWLTfQ2CuKmtVM';
-const bestNoFriendlyUrl ='https://drive.google.com/open?id=157ugVo13Bp7ibZ-knSui1Y3_gakC3ee-gj45cuTuggY';
-
 let subject = 'Crack en inferiores: :playerName';
-
-let normalBody='';
-let bestNoFriendlyBody='';
-let bestNoLeagueBody='';
-let bestNormalBody='';
+const urls = [
+	'https://drive.google.com/open?id=1K6zS2XX1adT0wgBDF-7K4DB79Uyo7RdUqJP_6RYzK44', // normal
+	'https://drive.google.com/open?id=157ugVo13Bp7ibZ-knSui1Y3_gakC3ee-gj45cuTuggY', // best no friendly
+	'https://drive.google.com/open?id=1NKpYDpocY2zp8EBLIZF_OjzpB-rXMeWLTfQ2CuKmtVM', // best no league
+	'https://drive.google.com/open?id=16d9QdobMJXkV_50qjcGQc_FDSWp4MQPhC04HULZKBeI'  // best normal
+];
+const bodies = ['', '', '', ''];
+const msgs = [
+	chrome.i18n.getMessage('normal'), 
+	chrome.i18n.getMessage('bestNoFriendly'), 
+	chrome.i18n.getMessage('bestNoLeague'),
+	chrome.i18n.getMessage('bestNormal')
+];
+let buttons;
 
 let player;
-let mailBody;
-let mailSubject;
+const mailBody = document.getElementById('ctl00_ctl00_CPContent_CPMain_ucEditorMain_txtBody');
+const mailSubject = document.getElementById('ctl00_ctl00_CPContent_CPMain_tbSubject');
+const container = document.createElement('DIV');
 
-let run = function() {
+const run = function() {
 	if(!player) {
 		return;
 	}
-
-	mailBody = document.getElementById('ctl00_ctl00_CPContent_CPMain_ucEditorMain_txtBody');
-	mailSubject = document.getElementById('ctl00_ctl00_CPContent_CPMain_tbSubject');
-
-	let container = document.createElement('DIV');
+	mailSubject.value = subject.replace(':playerName', player.name);
 	container.className = 'pasteMailContainer';
-    
-    createButton(chrome.i18n.getMessage('normal'), container, 'template1');
-	createButton(chrome.i18n.getMessage('bestNoFriendly'), container, 'template2');
-	createButton(chrome.i18n.getMessage('bestNoLeague'), container, 'template3');
-	createButton(chrome.i18n.getMessage('bestNormal'), container, 'template4');
+	hide(container)
+    buttons = msgs.map((message, index) => createButton(message, index));
 
 	let sendMailButton = document.getElementById('ctl00_ctl00_CPContent_CPMain_btnSendNew');
-	sendMailButton.addEventListener('click', function() {
-        clearPlayer();
-    });
+	sendMailButton.addEventListener('click', () => clearPlayer());
 };
 
-let sendMailAction = function(template) {
-    chrome.storage.local.get(['thread'], function(result) {
-    	let text;
-    	switch(template) {
-    		case 'template1':
-    		text = normalBody;
-    		break;
-    		case 'template2':
-    		text = bestNoFriendlyBody;
-    		break;
-    		case 'template3':
-    		text = bestNoLeagueBody;
-    		break;
-    		case 'template4':
-    		text = bestNormalBody;
-    		break
-    	}
+const hide = function(elm) {
+	elm.style.visibility = 'hidden';
+}
+
+const show = function(elm) {
+	elm.style.visibility = 'visible';
+}
+
+const sendMailAction = function(index) {
+    chrome.storage.local.get(['thread'], result => {
 		let thread = result.thread;
-		if(!thread || !text) {
+		if(!thread || !bodies[index]) {
 			return;
 		}
-		mailBody.value = text.normalize().replace('{0}', player.name).replace('{1}', '[youthplayerid=' + player.id + ']').replace('{4}', thread);
-		mailSubject.value = subject.replace(':playerName', player.name);
+		mailBody.value = bodies[index].normalize().replace('{0}', player.name).replace('{1}', '[youthplayerid=' + player.id + ']').replace('{4}', thread);
 	});
 };
 
-let createButton = function(message, container, className) {
+const createButton = function(message, index) {
 	let button = document.createElement('A');
+	hide(button);
 	button.attributes.role = 'button';
 	button.title = message;
-	button.className = 'pasteMailButton ' + className;
+	button.className = 'pasteMailButton';
+	button.id = 'template' + (index + 1);
 	button.innerHTML = '<img alt="' + message + '" src="/Img/Icons/transparent.gif">';
 	container.appendChild(button);
 	mailBody.parentNode.insertBefore(container, mailBody);
-	button.addEventListener('click', function() {
-        sendMailAction(className);
-    });
+	button.addEventListener('click', () => sendMailAction(index));
+    return button;
 };
 
-let clearPlayer = function() {
-	chrome.storage.local.set({"player": null}, function() {});
+const clearPlayer = function() {
+	chrome.storage.local.set({"player": null}, ()  => {});
 };
 
-fetch(normalUrl)
-.then((response) => response.text())
-.then(function(text) {
-	let txt = text.substring(text.indexOf('"s":"') + 5).split('"', 2);
-	normalBody = txt[0].replace(/\\n/g, '\n').replace(/\\u003d/, '=');
-});
-
-fetch(bestNormalUrl)
-.then((response) => response.text())
-.then(function(text) {
-	let txt = text.substring(text.indexOf('"s":"') + 5).split('"', 2);
-	bestNormalBody = txt[0].replace(/\\n/g, '\n').replace(/\\u003d/, '=');
-});
-
-fetch(bestNoLeagueUrl)
-.then((response) => response.text())
-.then(function(text) {
-	let txt = text.substring(text.indexOf('"s":"') + 5).split('"', 2);
-	bestNoLeagueBody = txt[0].replace(/\\n/g, '\n').replace(/\\u003d/, '=');
-});
-
-fetch(bestNoFriendlyUrl)
-.then((response) => response.text())
-.then(function(text) {
-	let txt = text.substring(text.indexOf('"s":"') + 5).split('"', 2);
-	bestNoFriendlyBody = txt[0].replace(/\\n/g, '\n').replace(/\\u003d/, '=');
-});
-
-chrome.storage.local.get(['player'], function(result) {
+chrome.storage.local.get(['player'], result => {
 	player = result.player;
 	run();
+});
+
+bodies.map((body, index) => {
+	fetch(urls[index])
+	.then(response => response.text())
+	.then(text => {
+		let txt = text.substring(text.indexOf('"s":"') + 5).split('"', 2);
+		show(container);
+		show(buttons[index]);
+		let result = txt[0].replace(/\\n/g, '\n').replace(/\\u003d/, '=');
+		bodies[index] = result;
+		return result;
+	});
 });
--- src/scripts/youthplayer.js
+++ src/scripts/youthplayer.js
@@ -1,34 +1,31 @@
 let userId;
 let thread;
 
-let run = function() {
-	let mainBody = document.getElementById('mainBody');
-	let button = document.createElement('A');
+const run = function() {
+	const mainBody = document.getElementById('mainBody');
+	const button = document.createElement('A');
 	button.attributes.role = 'button';
 	button.title = chrome.i18n.getMessage('sendmail');
-	button.className = 'sendMailButton';
 	button.id = 'sendMailButton';
 	button.innerHTML = '<img alt="' + chrome.i18n.getMessage('sendmail') + '" src="/Img/Icons/transparent.gif">';
 	mainBody.insertBefore(button, mainBody.childNodes[0]);
-	button.addEventListener('click', function() {
-        sendMailAction();
-    });
+	button.addEventListener('click', () => sendMailAction());
 };
 
-let sendMailAction = function() {
-	chrome.storage.local.get(['thread'], function(result) {
+const sendMailAction = function() {
+	chrome.storage.local.get(['thread'], result => {
 		thread = result.thread;
 	});
-	let player = document.getElementsByClassName('hasByline')[0].innerText;
-	let playerName = player.substring(0, player.lastIndexOf(' ')).trim();
-	let playerId = player.substring(player.indexOf('(') + 1, player.indexOf(')')).trim();
+	const player = document.getElementsByClassName('hasByline')[0].innerText;
+	const playerName = player.substring(0, player.lastIndexOf(' ')).trim();
+	const playerId = player.substring(player.indexOf('(') + 1, player.indexOf(')')).trim();
 
 	let managerUrl = getManagerUrl();
 	
 	if(managerUrl === '') {
 		return;
 	}
-	getUserId(managerUrl).then(function() {
+	getUserId(managerUrl).then(() => {
 		if(!userId) {
 			console.log(chrome.i18n.getMessage('noUserFound'));
 		}
@@ -40,64 +37,37 @@ let sendMailAction = function() {
 	
 }
 
-let getManagerUrl = function() {
-	let result = '';
-	let boxBodies = document.getElementsByClassName('boxBody');
-	let boxBodyChildren;
-	for(let i = 0; i < boxBodies.length; i++) {
-		if(boxBodies[i].innerHTML.indexOf('/Club/Manager') !== -1) {
-			boxBodyChildren = boxBodies[i].childNodes;
-			break;
-		}
-	}
-	let found = false;
-	for(let i = 0; i < boxBodyChildren.length; i++) {
-		if(boxBodyChildren[i].tagName === 'UL') {
-			let count = 0;
-			for(let li = 0; li < boxBodyChildren.length; li++) {
-				if(boxBodyChildren[i].childNodes[li].tagName == 'LI') {
-					if(count == 1) {
-						for(let a = 0; a < boxBodyChildren[i].childNodes[li].childNodes.length; a++) {
-							if(boxBodyChildren[i].childNodes[li].childNodes[a].tagName == 'A') {
-								result = boxBodyChildren[i].childNodes[li].childNodes[a].href;
-								return result;
-							}
-						}
-					}
-					count++;
-				}
-			}
-			break;
-		}
-	}
-	return result;
+const getManagerUrl = function() {
+	let result = document.querySelectorAll('div.subMenuBox > div.boxBody > ul > li > a[href*="Club/Manager"]')[0];
+	return result.href;
 };
 
-let savePlayer = function(playerName, playerId) {
+const savePlayer = function(playerName, playerId) {
 	let player = {"name": playerName, "id": playerId};
 	return chrome.storage.local.set({"player": player}, checkRedirect);
 };
 
-let checkRedirect = function() {
+const checkRedirect = function() {
 	if(thread && thread !== '' && userId) {
 		window.location.href = '/MyHattrick/Inbox/?actionType=newMail&userId=' + userId;
 	}
 };
 
-let getUserId = function(managerUrl) {
+const getUserId = function(managerUrl) {
 	return fetch(managerUrl)
-		.then((response) => response.text())
-	    .then(function(text) {
+		.then(response => response.text())
+	    .then(text => {
 	    	let parser = new DOMParser();
 		    let htmlDocument = parser.parseFromString(text, "text/html");
 		    let byLine = htmlDocument.documentElement.getElementsByClassName("hasByline")[0];
 		    userId = byLine.innerText.substring(byLine.innerText.indexOf('(') + 1, byLine.innerText.indexOf(')')).trim();
-	    });
+	    })
+	    .catch(err => console.log(err));
 }
 
-chrome.storage.local.get(['thread'], function(result) {
+chrome.storage.local.get(['thread'], result => {
 	if(!result.thread) {
-		chrome.storage.local.set({"thread":'17084291.1'}, function(){});
+		chrome.storage.local.set({"thread":'17084291.1'}, () => {});
 	}
 	run();
 });
