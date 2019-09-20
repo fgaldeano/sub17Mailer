512cdbcbb5c4416811526f0d2a7df121d2c21449
Facundo Galdeano <fgaldeano@gmail.com>
2019-09-19 21:15:00 -0300

    Fixed topyouth export

--- src/scripts/topyouth.js
+++ src/scripts/topyouth.js
@@ -23,33 +23,34 @@ const addExportButton = function() {
 };
 
 const getExportData = function() {
-    const header = [
-    	chrome.i18n.getMessage('csvHeader_name'), 
+  const header = [
+  	chrome.i18n.getMessage('csvHeader_name'), 
 		chrome.i18n.getMessage('csvHeader_id'), 
 		chrome.i18n.getMessage('csvHeader_years'), 
 		chrome.i18n.getMessage('csvHeader_days'), 
 		chrome.i18n.getMessage('csvHeader_toPromote'), 
 		chrome.i18n.getMessage('csvHeader_specialty'), 
 		chrome.i18n.getMessage('csvHeader_stars'), 
-		chrome.i18n.getMessage('csvHeader_position')
+		chrome.i18n.getMessage('csvHeader_position'),
+		chrome.i18n.getMessage('csvHeader_date')
 	];
-    let playerPosition = playerType.options[playerType.selectedIndex].text;
+  let playerPosition = playerType.options[playerType.selectedIndex].text;
 
 	csvContent = "data:text/csv;charset=utf-8," + header.join(',') + '\r\n';
 	for(let tr of results) {
-		let row = getRowFromTR(tr).join(',') + "," + playerPosition;
+		let row = getRowFromTR(tr, playerPosition, new Date()).join(',');
 		csvContent += row + '\r\n';
 	}
 };
 
-const getRowFromTR = function(tr) {
+const getRowFromTR = function(tr, position, now) {
 	let rowArray = [];
 	let index = 0;
 	for(let td of tr.querySelectorAll('td')) {
-		let images = td.querySelectorAll('img');
+		let stars = td.querySelector('.stars');
 		let playerLink = td.querySelector('a');
 		let innerText = td.innerText.trim();
-	    if(index === 0 && playerLink) {
+	  if(index === 0 && playerLink) {
 			rowArray.push(innerText);
 			rowArray.push(playerLink.href.substring(playerLink.href.indexOf('=') + 1));
 		}
@@ -61,28 +62,25 @@ const getRowFromTR = function(tr) {
 		if(index > 1 && index < 4) {
 			rowArray.push(innerText);
 		}
-		if(index === 4 && images.length > 0) {
-			rowArray.push(getNumberFromStars(images));
+		if(index === 4 && stars.childNodes.length > 0) {
+			rowArray.push(getNumberFromStars(stars));
 		}
 		index++;
-	}    
+	}
+	rowArray.push(position);
+	rowArray.push('' + (now.getMonth() + 1) + '/' + now.getDay() + '/' + now.getFullYear());
 	return rowArray;
 };
 
-const getNumberFromStars = function(images) {
+const getNumberFromStars = function(stars) {
 	let number = 0;
-	for(let img of images) {
-		if(img.className.indexOf('starBig') !== -1) {
-			number += 5;
-		}
-		if(img.className.indexOf('starWhole') !== -1) {
-			number += 1;
-		}
-		if(img.className.indexOf('starHalf') !== -1) {
-			number += 0.5;
-		}
+	let intPart = stars.querySelector('.stars-full');
+	let decimalPart = stars.querySelector('.stars-half');
+	if(decimalPart) {
+		number = parseFloat(intPart.innerText + decimalPart.innerText);
+	} else {
+		number = parseInt(intPart.innerText);
 	}
-
 	return number;
 };
 
