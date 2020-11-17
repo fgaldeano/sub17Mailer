42a8b50cb70594d0dc2d5f2628aedd695dbbe787
Mauro Aranda <maurooaranda@gmail.com>
2020-11-17 20:27:11 -0300

    Be more liberal in what you accept
    Regex match the thread's name, to detect things like 'Sub 17',
    'sub17', 'sub 17', etc.

--- src/scripts/youthmail.js
+++ src/scripts/youthmail.js
@@ -108,8 +108,9 @@ chrome.storage.local.get(['thread'], result => {
 			let result = htmlDocument.querySelectorAll('div.mainConf > div.boxHead > h2 > span.float_left > a[href*="/Forum/Read.aspx"]');
 			threadSpan.title = thread;
 			
+			let regex = /\sub\s*17\]/i;
 			for(title of result) {
-				if(title.innerText.indexOf('Sub17') !== -1) {
+				if(regex.test(title.innerText)) {
 					threadSpan.innerText = title.innerText;
 					break;
 				}
